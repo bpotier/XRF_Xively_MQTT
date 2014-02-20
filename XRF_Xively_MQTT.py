5d916ab96b69cf988428411832ad1c4bd98cbb18
bpotier <benoit.potier@gmail.com>
2014-02-20 10:46:02 +0100

    Modify script name

--- XRF_Xively_MQTT.py
+++ XRF_Xively_MQTT.py
@@ -7,50 +7,45 @@ import datetime
 import re
 import eeml
 
-
 broker = "127.0.0.1"
 tcpport = 1883
 baud = 9600
 port = '/dev/ttyAMA0'
 ser = serial.Serial(port, baud)
 
-#cosm details
-XIVELYMQTTServer = "api.xively.com"
+#XIVELY details
+#Change values according to your XIVELY config
+######################################################################
+XIVELYMQTTServer = "api.xively.com" 
 XIVELYAPIKey ="IvUMMwND4OsBfXhO0WmIq1XwbOpGs1HGHTEkLw9KTTqpdc18"
 XIVELYFeed = 41913791
 XIVELYUrl = '/v2/feeds/{feednum}.xml' .format(feednum = XIVELYFeed)
+######################################################################
 
 mypid = os.getpid()
 client_uniq = "pubclient_"+str(mypid)
 mqttc = mosquitto.Mosquitto(client_uniq)
 
-#connect to broker
-#print "Connect to MQTT Broker..."
+#Connect to broker
 mqttc.connect(broker, tcpport, 60, True)
 
-#remain connected and publish
+#Remain connected and publish
 while mqttc.loop() == 0:
         llapMsg = ser.read(12)
         mqttc.publish("TempSensors", llapMsg)
         devID = llapMsg[1:3]
-#       xively = eeml.Pachube(XIVELYUrl, XIVELYAPIKey)
-#       temp = llapMsg[7:12]
 
         if re.search("a01TMPA", llapMsg):
                 temp = llapMsg[7:12]
                 xively = eeml.Pachube(XIVELYUrl, XIVELYAPIKey)
-                #send data to XIVELY
                 xively.update([eeml.Data(devID + "_Exterieur", temp, unit=eeml.Celsius())])
-#                print("XIVELY updated "+devID+"_Exterieur with value: "+temp)
 
         if re.search("a02TMPA", llapMsg):
                 temp = llapMsg[7:12]
                 xively = eeml.Pachube(XIVELYUrl, XIVELYAPIKey)
-                #send data to XIVELY
                 xively.update([eeml.Data(devID + "_Chambre", temp, unit=eeml.Celsius())])
-#                print("XIVELY updated "+devID+"_Chambre with value: "+temp)
 
-        #Push data to cosm
+        #Push data to XIVELY
         try:
                 xively.put()
 
